3c966c697c1fe4c2087dc56654aa9f18d5123614
vrym2 <vardhan609@gmail.com>
2023-11-22 22:15:33 +0000

    reformatting

--- opencosmos/copernicus/__init__.py
+++ opencosmos/copernicus/__init__.py
@@ -4,3 +4,4 @@ from .cog import cloud_optimised_geotiff
 from .open_search import copernicus_api_search
 from .product_download import copernicus_data_download
 from .sentinel_bands import sentinel2_bands
+from .img_format import sentinel_image_webp
--- opencosmos/utils/__init__.py
+++ opencosmos/utils/__init__.py
@@ -1,5 +1,4 @@
 """Importing functions"""
-from .img_format import sentinel_image_webp
 from .loading_animation import Loader
 from .plot import plot_image
 from .zip_download import download_data_from_url
